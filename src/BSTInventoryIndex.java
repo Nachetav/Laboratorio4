85c02d923c8f63bd0af17c6bf291de1e4c0b7873
dvzx <daniel.velasquez1@mail.udp.cl>
2026-06-11 15:49:12 -0400

    Add files via upload

---
+++ src/BSTInventoryIndex.java
@@ -0,0 +1,7 @@
+import edu.princeton.cs.algs4.BST;
+
+public class BSTInventoryIndex {
+    private BST<Integer, InventoryItem> st;
+
+    //hacer que los metodos sean del BST original de princeton
+}
---
+++ src/Experiment.java
@@ -0,0 +1,5 @@
+
+//clase encargada de ejecutar los metodos de BST y RDBST para luego contar los tiempos 
+public class Experiment {
+
+}
